a918754ee4b2cc1982bd750522fd1fa2c536a740
gracielamendezrojas <mmendezr@ucenfotec.ac.cr>
2024-04-18 15:01:09 -0600

    WhatsApp text

--- src/Components/WhatsAppIconComponent.js
+++ src/Components/WhatsAppIconComponent.js
@@ -1,7 +1,18 @@
 import WAlogo from '../Images/WAIcon.png';
+import { useContext } from "react";
+import { LanguageContext } from "../Context/Language";
 function WAIconComponent(){
+    const { language } = useContext(LanguageContext);
+    const labels = {
+      ES: {
+        msg: "https://wa.me/+50684451547?text=¡Hola! Quiero comunicarme contigo. Mi nombre es...",
+      },
+      EN: {
+        msg: "https://wa.me/+50684451547?text=Hi! I want to reach out to you. My name is...",
+      },
+    };
     return(
-        <a href="https://wa.me/+50684451547?text=Hi! I need to contact you." target='_blank'>
+        <a href={labels[language].msg} target='_blank'>
             <img src={WAlogo} className=" hover:animate-pulse w-20 fixed left-100 right-8 bottom-8"/>
         </a>
     ); 
