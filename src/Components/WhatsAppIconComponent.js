faa37339e1cd71badc8c9af668a9c1d3196b58b6
gracielamendezrojas <mmendezr@ucenfotec.ac.cr>
2024-04-18 14:53:37 -0600

    WhatsApp function

--- src/Components/WhatsAppIconComponent.js
+++ src/Components/WhatsAppIconComponent.js
@@ -1,9 +1,9 @@
 import WAlogo from '../Images/WAIcon.png';
 function WAIconComponent(){
     return(
-        <button>
+        <a href="https://wa.me/+50684451547?text=Hi! I need to contact you." target='_blank'>
             <img src={WAlogo} className=" hover:animate-pulse w-20 fixed left-100 right-8 bottom-8"/>
-        </button>
+        </a>
     ); 
 }
 export default WAIconComponent; 
